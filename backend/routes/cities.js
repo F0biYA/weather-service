1468df0c9f3a61115aa229366f37ae977476c685
F0BiYa <fobiya1982@yandex.ru>
2022-09-20 17:55:38 +0300

    readme

--- backend/routes/cities.js
+++ backend/routes/cities.js
@@ -3,8 +3,6 @@ const router = require('express').Router();
 const { getCities, createCity } = require('../controllers/cities');
 
 router.get('/', getCities);
-
-// POST /movies создаёт фильм
 router.post('/', createCity);
 
 module.exports = router;
